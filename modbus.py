588ecc534a47fe302f7df85aa0b89807575785be
Domenico Barra <domenico@adamantic.io>
2019-05-17 15:38:27 +0200

    fix floating

--- modbus.py
+++ modbus.py
@@ -11,23 +11,23 @@ def get_ws10_readings(ip, port):
         message = tcp.read_input_registers(1, 0, 80)
         response = tcp.send_message(message, sock)
         payload = {
-            'temperature': response[19] / 10,
-            'humidity': response[29] / 10,
-            'pressure': response[37] / 10,
-            'windspeed_kmh': response[49] / 10,
-            'wind_direction': response[54] / 10,
-            'precipitation': response[59] / 10,
-            'brightness': response[75] / 10,
-            'twilight': response[76] / 10,
+            'temperature': float(response[19]) / 10,
+            'humidity': float(response[29]) / 10,
+            'pressure': float(response[37]) / 10,
+            'windspeed_kmh': float(response[49]) / 10,
+            'wind_direction': float(response[54]) / 10,
+            'precipitation': float(response[59]) / 10,
+            'brightness': float(response[75]) / 10,
+            'twilight': float(response[76]) / 10
         }
         message = tcp.read_input_registers(1, 140, 10)
         response = tcp.send_message(message, sock)
         payload.update({
-            'lon': response[2] / 100,
-            'lat': response[3] / 10,
-            'alt': response[4],
-            'gpsnum': response[7],
-            'gpslock': response[8]
+            'lon': float(response[2]) / 100,
+            'lat': float(response[3]) / 100,
+            'alt': float(response[4]),
+            'gpsnum': float(response[7]),
+            'gpslock': float(response[8])
         })
         return payload
     finally:
